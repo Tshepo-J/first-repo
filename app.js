69850e3df55248d75ee201ec270d31475eb33e2f
Tshepo-J <jankutshepo@gmail.com>
2024-11-27 11:03:31 -0800

    Add you tasks

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-// npm - global command, comes with node
-// npm --version
+const { readFile } = require('fs')
+const { result } = require('lodash')
 
-// local dependency - use it only in this particulaer project
-// npm i <packageName>
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo install -g <packageName> (mac)
+const getText = (path) =>{
+    return new Promise((resolve,reject)=>{
+        readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data)
+            }
+        })
+    })
+}
 
-// package.json - manifest file (store important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-const _ = require('lodash')
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("Hello world");
-
+getText('./content/first.txt').then(result => console.log(result)).catch(err => console.log(err))
